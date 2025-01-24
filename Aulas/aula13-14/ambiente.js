cd28b15ac474c92bb6265bc11b9b0d06a65ccc82
Junior <fabjuniorcc@gmail.com>
2025-01-24 06:41:33 -0300

    created first half exercice

---
+++ Aulas/aula13-14/ambiente.js
@@ -0,0 +1,20 @@
+i = 1;
+
+/*
+    while (i <= 5) {
+        console.log('Passo ' + i);
+        i++;
+    }
+*/
+console.log('Iniciando');
+do{
+    console.log('Passo ' + i);
+    i++;
+} while (i <= 5);
+
+// Parte 2 (aula 14)
+console.log('Parte 2');
+for (var c = 1; c <= 10; c++) {
+    console.log('Passo ' + c);
+}
+console.log('Fim!');
